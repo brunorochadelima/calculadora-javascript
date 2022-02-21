8463df1e83f1048208fd1479f56e86e3cc59fb5d
Bruno Rocha de Lima <brunorocha33@live.com>
2022-02-21 14:35:59 -0300

    Criando Funções

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function calcular(tipo, valor) {
+  if (tipo === 'acao') {
+    
+  } else if (tipo === 'valor') {
+    var resultado = document.getElementById('tela_resultado').value;
+    resultado = "valor"
+  }
+
+  conso
+}
