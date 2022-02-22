3fa99aed798bfdd23e37968ea9b5dde4da7fde69
Bruno Rocha de Lima <brunorocha33@live.com>
2022-02-22 13:30:44 -0300

    Trabalhando Responsividade CSS

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function calcular(tipo, valor) {
     }
     
     if (valor === "="){
-      var valor_campo = eval(document.getElementById("tela_resultado").value)
+      var valor_campo = eval(document.getElementById("tela_resultado").value) //Eval converte uma string em números calculáveis
       valor_campo = document.getElementById("tela_resultado").value = valor_campo
     }
 
